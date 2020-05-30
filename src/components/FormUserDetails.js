ee932339c18a92cad90d6e42a95769dfb6e4f985
Temirlan <bamblebee988@gmail.com>
2020-05-30 22:59:50 +0300

    Add first step

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+
+export class FormUserDetails extends Component {
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  render() {
+    const { values, handleChange } = this.props;
+
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter User Details" />
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange("LastName")}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Email Name"
+            floatingLabelText="Email Name"
+            onChange={handleChange("email")}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            styles={styles.button}
+            onClick={this.continue}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15,
+  },
+};
+
+export default FormUserDetails;
